94fe2543140dfe1eba0595f77cc40b59a7b81f62
Anna-Sergeevna <4042808@gmail.com>
2021-10-29 19:50:51 +0300

    Добавляет 1 и 2 задание без стилей

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import Profile from 'components/Profile/Profile';
+import user from 'components/Profile/user.json';
+
+import Statistics from 'components/Statistics/Statistics';
+import statisticalData from 'components/Statistics/statistical-data.json';
+
+export default function App() {
+  return (
+    <div>
+      <Profile
+        key={user.tag}
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <div>
+        <Statistics title="Upload stats" stats={statisticalData} />
+        <Statistics stats={statisticalData} />
+      </div>
+    </div>
+  );
+}
